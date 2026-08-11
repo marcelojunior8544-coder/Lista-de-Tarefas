f563d2986f7244bce86ab662c64bcda43072e4a9
Marcelo Junior <marcelojunior8544@gmail.com>
2026-08-11 11:54:06 -0300

    Transformar em Class: ToDoList

--- lista.py
+++ lista.py
@@ -1,64 +1,76 @@
-def menu():
-    print('---Lista de Tarefas---')
-    print('1 - Adicionar tarefa')
-    print('2 - Exibir tarefas')
-    print('3 - Remover tarefa')
-    print('4 - Sair')
+class ToDoList:
+    def __init__(self):
+        self.tarefas = []
 
+    @staticmethod
+    def menu():
+        print('--- Lista de Tarefas ---')
+        print('1 - Adicionar tarefa')
+        print('2 - Exibir tarefas')
+        print('3 - Remover tarefa')
+        print('4 - Sair')
 
-def adicionar_tarefa(lista):
-    tarefa = str(input('Digite a tarefa: ')).strip()
-    if tarefa:
-        lista.append(tarefa)
+    def adicionar_tarefa(self):
+        tarefa = input('Digite a tarefa: ').strip()
+
+        if not tarefa:
+            print('A tarefa não pode ficar sem nome!')
+            return
+
+        self.tarefas.append(tarefa)
         print(f'Tarefa "{tarefa}" adicionada com sucesso!')
-    else:
-        print('A tarefa não pode ficar sem nome!')
 
+    def exibir_tarefas(self):
+        if not self.tarefas:
+            print('Nenhuma tarefa cadastrada.')
+            return
 
-def exibir_tarefas(lista):
-    if not lista:
-        print('Nenhuma tarefa cadastrada.')
-    else:
-        print('\n---Lista de Tarefas---')
-        for i, tarefa in enumerate(lista, start=1):
-            print(f'{i} - {tarefa}')
+        print('\n--- Lista de Tarefas ---')
+        for indice, tarefa in enumerate(self.tarefas, start=1):
+            print(f'{indice} - {tarefa}')
 
+    def remover_tarefa(self):
+        if not self.tarefas:
+            print('Nenhuma tarefa cadastrada.')
+            return
 
-def remover_tarefa(lista):
-    exibir_tarefas(lista)
-    if lista:
+        self.exibir_tarefas()
+
         try:
             numero = int(input('Digite o número da tarefa que deseja remover: '))
-            if 1 <= numero <= len(lista):
-                removida = lista.pop(numero - 1)
-                print(f'Tarefa "{removida}" removida com sucesso!')
-            else:
-                print('Número inválido. Tente novamente.')
         except ValueError:
             print('Entrada inválida. Por favor, digite um número válido.')
+            return
 
+        if not 1 <= numero <= len(self.tarefas):
+            print('Número inválido. Tente novamente.')
+            return
 
-def sistema():
-    tarefas = list()
-    while True:
-        menu()
-        try:
-            opcao = input('\nEscolha uma opção: ')
-        except EOFError:
-            print('\nEntrada inválida. Encerrando o sistema...')
-            break
-        if opcao == '1':
-            adicionar_tarefa(tarefas)
-        elif opcao == '2':
-            exibir_tarefas(tarefas)
-        elif opcao == '3':
-            remover_tarefa(tarefas)
-        elif opcao == '4':
-            print('Encerrando o sistema...')
-            break
-        else:
-            print('Opção inválida. Tente novamente.')
+        tarefa_removida = self.tarefas.pop(numero - 1)
+        print(f'Tarefa "{tarefa_removida}" removida com sucesso!')
+
+    def executar(self):
+        while True:
+            self.menu()
+
+            try:
+                opcao = input('\nEscolha uma opção: ').strip()
+            except EOFError:
+                print('\nEntrada inválida. Encerrando o sistema...')
+                break
+
+            if opcao == '1':
+                self.adicionar_tarefa()
+            elif opcao == '2':
+                self.exibir_tarefas()
+            elif opcao == '3':
+                self.remover_tarefa()
+            elif opcao == '4':
+                print('Encerrando o sistema...')
+                break
+            else:
+                print('Opção inválida. Tente novamente.')
 
 
 if __name__ == '__main__':
-    sistema()
+    ToDoList().executar()
